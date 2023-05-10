64fade3c1dd83a45a42917b3084f57638163bd21
miurachikaa <chikaa719@gmail.com>
2023-05-10 09:50:39 +0900

    Initial commit

---
+++ src/main/java/in/techcamp/issueapp2/IssueaApp2Application.java
@@ -0,0 +1,13 @@
+package in.techcamp.issueapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueaApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueaApp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/in/techcamp/issueapp2/IssueaApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.techcamp.issueapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueaApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
