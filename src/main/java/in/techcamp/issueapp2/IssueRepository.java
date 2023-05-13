9667a2c59991dd347bae59334fbb5beb2aa4097a
miurachikaa <chikaa719@gmail.com>
2023-05-13 10:57:39 +0900

    保存機能

---
+++ src/main/java/in/techcamp/issueapp2/IssueRepository.java
@@ -0,0 +1,11 @@
+package in.techcamp.issueapp2;
+
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface IssueRepository {
+    @Insert("insert into issues (title, content, period, importance) values (#{title}, #{content}, #{period}, #{importance})")
+    void insert(String title, String content, String period, char importance);
+}
