0f513ef86fd13249a38c768d49aeff9d0572ae66
miurachikaa <chikaa719@gmail.com>
2023-05-15 09:27:47 +0900

    一覧表示

--- src/main/java/in/techcamp/issueapp2/IssueRepository.java
+++ src/main/java/in/techcamp/issueapp2/IssueRepository.java
@@ -3,9 +3,15 @@ package in.techcamp.issueapp2;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface IssueRepository {
     @Insert("insert into issues (title, content, period, importance) values (#{title}, #{content}, #{period}, #{importance})")
     void insert(String title, String content, String period, char importance);
+
+    @Select("select * from issues")
+    List<IssueEntity> findAll();
 }
